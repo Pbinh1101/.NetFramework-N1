33bbc32bf52cc4942af489f5b649b11ab89d70b2
unknown <binhnguyen110120@gmail.com>
2021-10-03 20:58:22 +0700

    first commit

---
+++ BasicWinForm/Entities/Historystudy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWinform.Entities
+{
+    public class Historystudy
+    {
+        public string Cap { get; set; }
+        public string Truonghoc { get; set; }
+        public float DiemTB { get; set; }
+        public string Hanhkiem { get; set; }
+
+        public static List<Historystudy> GetList()
+        {
+            var ds = new List<Historystudy>();
+            ds.Add(new Historystudy
+            {
+                Cap = "Cấp 1",
+                Truonghoc = "Trường Tiểu học số 2 Hương Xuân",
+                DiemTB = 8f,
+                Hanhkiem = "Tốt"
+            });
+            ds.Add(new Historystudy
+            {
+                Cap = "Cấp 2",
+                Truonghoc = "Trường Trung học cơ sở Nguyễn Khánh Toàn",
+                DiemTB = 7.9f,
+                Hanhkiem = "Tốt"
+            });
+            ds.Add(new Historystudy
+            {
+                Cap = "Cấp 3",
+                Truonghoc = "Trường Trung học phổ thông Đăng Huy Trứ",
+                DiemTB = 8.0f,
+                Hanhkiem = "Tốt"
+            });
+            return ds;
+        }
+
+        public static Historystudy Get(string cap)
+        {
+            var dbHistory = GetList();
+            var history = dbHistory.Where(p => p.Cap == cap).FirstOrDefault();
+            return history;
+        }
+    }
+}
---
+++ BasicWinForm/frmmain.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicWinForm
+{
+    public partial class frmmain : Form
+    {
+        public frmmain()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncong_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var sothunhat = txtsothunhat.Text;
+                double temp = 0, nsothunhat = 0, nsothuhai = 0;
+                if (double.TryParse(sothunhat, out temp)) 
+                    nsothunhat = Convert.ToDouble(sothunhat);
+                else
+                {
+                    MessageBox.Show(
+                    $"Vui lòng nhập lại số thứ nhất",
+                    "thông báo lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                     return;
+
+                    //}
+                }
+                var sothuhai = txtsothuhai.Text;
+                if (Double.TryParse(sothuhai, out temp)) 
+                     nsothuhai = double.Parse(sothuhai);
+                 else
+                 {
+                    MessageBox.Show(
+                    $"Vui lòng nhập lại số thứ hai",
+                    "thông báo lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                    return;
+
+                }
+                var ketqua = nsothunhat + nsothuhai;
+                lblketqua.Text = ketqua.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    $"Lỗi định dạng : Vui lòng nhập lại số . Chi tiết lỗi :{ex.Message} ",
+                    "Thông báo lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                ) ;
+            }
+
+        }
+
+        private void btntru_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var sothunhat = txtsothunhat.Text;
+                double temp = 0, nsothunhat = 0, nsothuhai = 0;
+                if (double.TryParse(sothunhat, out temp))
+                    nsothunhat = Convert.ToDouble(sothunhat);
+                else
+                {
+                    MessageBox.Show(
+                    $"Vui lòng nhập lại số thứ nhất",
+                    "thông báo lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                    return;
+                }
+                var sothuhai = txtsothuhai.Text;
+                if (Double.TryParse(sothuhai, out temp))
+                    nsothuhai = double.Parse(sothuhai);
+                else
+                {
+                    MessageBox.Show(
+                    $"Vui lòng nhập lại số thứ hai",
+                    "thông báo lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                    return;
+
+                }
+                var ketqua = nsothunhat - nsothuhai;
+                lblketqua.Text = ketqua.ToString("N0");
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(
+                    $"Lỗi định dạng : Vui lòng nhập lại số . Chi tiết lỗi :{ex.Message} ",
+                    "Thông báo lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show(
+                 $"Lỗi tràn số  : Vui lòng nhập lại số ngắn hơn . Chi tiết lỗi :{ex.Message} ",
+                 "Thông báo lỗi",
+                 MessageBoxButtons.OK,
+                 MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                 $"Lỗi, Chi tiết lỗi :{ex.Message} ",
+                 "Thông báo lỗi",
+                 MessageBoxButtons.OK,
+                 MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnnhan_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                var sothunhat = txtsothunhat.Text;
+                double temp = 0, nsothunhat = 0, nsothuhai = 0;
+                if (double.TryParse(sothunhat, out temp))
+                    nsothunhat = Convert.ToDouble(sothunhat);
+                else
+                {
+                    MessageBox.Show(
+                    $"Vui lòng nhập lại số thứ nhất",
+                    "thông báo lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                    return;
+                }
+                var sothuhai = txtsothuhai.Text;
+                if (Double.TryParse(sothuhai, out temp))
+                    nsothuhai = double.Parse(sothuhai);
+                else
+                {
+                    MessageBox.Show(
+                    $"Vui lòng nhập lại số thứ hai",
+                    "thông báo lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                    return;
+
+                }
+                var ketqua = nsothunhat * nsothuhai;
+                lblketqua.Text = ketqua.ToString("N0");
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(
+                    $"Lỗi định dạng : Vui lòng nhập lại số . Chi tiết lỗi :{ex.Message} ",
+                    "Thông báo lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show(
+                 $"Lỗi tràn số  : Vui lòng nhập lại số ngắn hơn . Chi tiết lỗi :{ex.Message} ",
+                 "Thông báo lỗi",
+                 MessageBoxButtons.OK,
+                 MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                 $"Lỗi, Chi tiết lỗi :{ex.Message} ",
+                 "Thông báo lỗi",
+                 MessageBoxButtons.OK,
+                 MessageBoxIcon.Error);
+            }
+        
+        }
+
+        private void btnchia_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var sothunhat = txtsothunhat.Text;
+                double temp = 0, nsothunhat = 0, nsothuhai = 0;
+                if (double.TryParse(sothunhat, out temp))
+                    nsothunhat = Convert.ToDouble(sothunhat);
+                else
+                {
+                    MessageBox.Show(
+                    $"Vui lòng nhập lại số thứ nhất",
+                    "thông báo lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                    return;
+                }
+                var sothuhai = txtsothuhai.Text;
+                if (Double.TryParse(sothuhai, out temp))
+                    nsothuhai = double.Parse(sothuhai);
+                else
+                {
+                    MessageBox.Show(
+                    $"Vui lòng nhập lại số thứ hai",
+                    "thông báo lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                    return;
+
+                }
+                var ketqua = nsothunhat / nsothuhai;
+                lblketqua.Text = ketqua.ToString("N0");
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(
+                    $"Lỗi định dạng : Vui lòng nhập lại số . Chi tiết lỗi :{ex.Message} ",
+                    "Thông báo lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show(
+                 $"Lỗi tràn số  : Vui lòng nhập lại số ngắn hơn . Chi tiết lỗi :{ex.Message} ",
+                 "Thông báo lỗi",
+                 MessageBoxButtons.OK,
+                 MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                 $"Lỗi, Chi tiết lỗi :{ex.Message} ",
+                 "Thông báo lỗi",
+                 MessageBoxButtons.OK,
+                 MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+    }
